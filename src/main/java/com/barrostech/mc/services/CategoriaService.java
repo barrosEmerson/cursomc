10a17885ce69a002b90f41ea6d3157f9b8c2d277
Emerson Barros <kaua54@hotmail.com>
2018-03-14 19:15:35 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/barrostech/mc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.barrostech.mc.services;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.barrostech.mc.domain.Categoria;
+import com.barrostech.mc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+		
+	}
+
+}
